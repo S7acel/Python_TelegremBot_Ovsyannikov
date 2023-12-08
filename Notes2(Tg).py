fbeefca236e36fa97617cff38ae9ce02fbfaa87b
S7acel <ovsyannikovmark11@gmail.com>
2023-12-08 14:52:22 +0500

    Notes2 <-- file with changed logic and Notes tg files added!

--- Notes2(Tg).py
+++ Notes2(Tg).py
@@ -1,23 +1,73 @@
 import telegram
 from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
 from secrets import API_TOKEN
-from Notes2 import build_note, read_note, edit_note
+from Notes2 import build_note, read_note, edit_note, delete_note, display_notes, delete_all_notes
 
 nt_nm = ''
 
 
+def reset(context):  # удаление информации после выполнения функции
+    context.user_data['command'] = None
+    context.user_data['note_name'] = None
+
+
 def greeting(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text='привет!)')
+    text = 'Привет, я NoteWizard - твой личный помощник для создания заметок. Я могу помочь тебе создавать заметки и ' \
+           'сохранять их в отдельных папках. Мои команды включают в себя:\n\n/create - ' \
+           'создать новую заметку.\n/read - прочитать существующую заметку.\n/edit - изменить существующую ' \
+           'заметку.\n/delete - удалить существующую заметку.\n/display - отобразить список всех ' \
+           'заметок.\n/delete_all_notes - удалить все заметки.\n\nЧтобы узнать больше о моих возможностях, ' \
+           'используйте команду /help. Давай начнем работу!'
+    context.bot.send_message(chat_id=update.message.chat_id, text=text)
+
+def help_handler(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Ух-ты! На этом весь мой функционал закончился! :( '
+                                                                  '\n Если есть предложения, или рекомендации по '
+                                                                  'улучшению бота, то напишите сюда --> @Markceil\n '
+                                                                  'Был бы очень благодарен 🤩')
+
+def delete_all_notes_handler(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Вы уверены? \n Да - "Y" Нет - \"N\" ')
+    context.user_data['command'] = 'delete_all_notes_handler'
 
 
-def message_handler(update, context):  # обработчик сообщений, применение словаря для функций
+def create_note_handler(update, context):  # спрашивание заметки, перенаправление на функцию создания словаря
+    context.bot.send_message(update.message.chat_id, text='Введите название заметки')
+    context.user_data['command'] = 'name_note_handler'
+
+
+def display_notes_handler(update, context):
+    notes_list = display_notes(update.message.chat_id)
+    if notes_list:
+        for i in notes_list:
+            context.bot.send_message(chat_id=update.message.chat_id, text=str(i))
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Файлов пока что нет')
+
+
+def read_note_handler(update, context):  # функция-обработчик спрашивание название файла
+    context.bot.send_message(update.message.chat_id, text='Введите название заметки')
+    context.user_data['command'] = 'read_note_handler'
+
+
+def edit_note_handler(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=f'Введите название заметки')
+    context.user_data['command'] = 'edit_note_handler'
+
+
+def delete_note_handler(update, context):  # функция обработчик удаление заметки
+    context.bot.send_message(chat_id=update.message.chat_id, text='Какую заметку вы хотите удалить?')
+    context.user_data['command'] = 'delete_note'
+
+
+def message_handler(update, context):  # обработчик сообщений, применение словаря для функций для определения функции
     # Создание заметки
     global nt_nm
     if context.user_data['command'] == 'create_note_handler':
         result = build_note(nt_nm, update.message.text, str(update.message.chat_id))
         context.bot.send_message(chat_id=update.message.chat_id, text=result)
         nt_nm = ''
-        context.user_data['command'] = None
+        reset(context)
     elif context.user_data['command'] == 'name_note_handler':
         nt_nm = str(update.message.text)  # Присваивание имени глобальной переменной nt_nm
         context.bot.send_message(chat_id=update.message.chat_id, text='Введите содержимое заметки')
@@ -27,7 +77,7 @@ def message_handler(update, context):  # обработчик сообщений
     elif context.user_data['command'] == 'read_note_handler':
         result = read_note(update.message.text, update.message.chat_id)
         context.bot.send_message(chat_id=update.message.chat_id, text=result)
-        context.user_data['command'] = None
+        reset(context)
     # Редактирование заметки
     elif context.user_data['command'] == 'edit_note_handler':
         context.user_data['note_name'] = update.message.text  # реализация запоминания заметки без глобальной функции
@@ -36,19 +86,21 @@ def message_handler(update, context):  # обработчик сообщений
     elif context.user_data['command'] == 'final_edit_of_note':
         result = edit_note(context.user_data['note_name'], str(update.message.chat_id), update.message.text)
         context.bot.send_message(chat_id=update.message.chat_id, text=result)
-def create_note_handler(update, context):  # спрашивание заметки, перенаправление на функцию создания словаря
-    context.bot.send_message(update.message.chat_id, text='Введите название заметки')
-    context.user_data['command'] = 'name_note_handler'
-
-
-def edit_note_handler(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=f'Введите название заметки')
-    context.user_data['command'] = 'edit_note_handler'
-
-
-def read_note_handler(update, context):
-    context.bot.send_message(update.message.chat_id, text='Введите название заметки')
-    context.user_data['command'] = 'read_note_handler'
+        reset(context)
+    elif context.user_data['command'] == 'delete_all_notes_handler':
+        if update.message.text == 'Y':
+            result = delete_all_notes(update.message.chat_id)
+            context.bot.send_message(chat_id=update.message.chat_id, text=result)
+            reset(context)
+        else:
+            context.bot.send_message(chat_id=update.message.chat_id, text='Действие отменено!')
+            reset(context)
+    # Удаление заметки
+    elif context.user_data['command'] == 'delete_note':
+        result = delete_note(update.message.text, update.message.chat_id)
+        context.bot.send_message(chat_id=update.message.chat_id, text=result)
+        reset(context)
+    # Удаление заметки
 
 
 def main():
@@ -58,6 +110,10 @@ def main():
     dispatcher.add_handler(CommandHandler('read', read_note_handler))
     dispatcher.add_handler(CommandHandler('create', create_note_handler))
     dispatcher.add_handler(CommandHandler('edit', edit_note_handler))
+    dispatcher.add_handler(CommandHandler('delete', delete_note_handler))
+    dispatcher.add_handler(CommandHandler('display', display_notes_handler))
+    dispatcher.add_handler(CommandHandler('delete_all_notes', delete_all_notes_handler))
+    dispatcher.add_handler(CommandHandler('help', help_handler))
     dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
     updater.start_polling()
 
--- Notes2.py
+++ Notes2.py
@@ -1,7 +1,8 @@
 import os
 
+path = f'C:/Users/user/PycharmProjects/Python_TelegremBot_Ovsyannikov/Notes/'  # директория файла пользователей
 
-path = f'C:/Users/user/PycharmProjects/Python_TelegremBot_Ovsyannikov/Notes/'  # директория файла пользователя
+
 def check_extension(f):  # Проверка на наличие '.txt' в конце
     try:
         if not f.endswith('.txt'):
@@ -43,16 +44,6 @@ def read_note(note_name, chat_id):  # Прочтение файла
         return f'Произошла ошибка {f}'
 
 
-
-def create_note():  # создание файла
-    try:
-        Name = input('Введите название файла\n')
-        Text = input('Введите текст\n')
-        build_note(Name, Text)
-    except:
-        print("Произошла ошибка.")
-
-
 def edit_note(name, chat_id, new_text):  # редактирование заметки
     try:
         name = check_extension(name)
@@ -66,93 +57,36 @@ def edit_note(name, chat_id, new_text):  # редактирование заме
         return f'Произошла ошибка {err}'
 
 
-def delete_note(name):  # Удаление заметки
+def delete_note(name, chat_id):  # Удаление заметки
     try:
         name = check_extension(name)
-        if os.path.isfile(name):
-            os.remove(name)
-            print(f'Заметка {name} удалена успешно')
+        if os.path.isfile(f'{path}/{chat_id}/{name}'):
+            os.remove(f'{path}/{chat_id}/{name}')
+            return (f'Заметка {name} удалена успешно')
         else:
-            print('Нельзя удалить то, чего и так нет.')
-    except:
-        print('Произошла ошибка')
+            return ('Нельзя удалить то, чего и так нет.')
+    except Exception as err:
+        return (f'Произошла ошибка {err}')
 
 
-def display_notes():  # Вывод названий файлов в порядке увеличения длины
+def display_notes(chat_id):  # Вывод названий файлов в порядке увеличения длины
     try:
-        notes = [note for note in os.listdir() if note.endswith('.txt')]
+        notes = os.listdir(f'{path}/{chat_id}')
         reversed_notes = sorted(notes, key=len)
-        if notes:
+        return reversed_notes
+    except Exception as err:
+        print(f'Произошла ошибка {err}')
+
+
+def delete_all_notes(chat_id):  # Создание списка всех файлов
+    try:
+        notes = os.listdir(f'{path}/{chat_id}')
+        reversed_notes = sorted(notes, key=len)
+        if reversed_notes:
             for i in reversed_notes:
-                print(i)
+                delete_note(i, chat_id)
+            return f'Все заметки удалены успешно! ({", ".join(reversed_notes)})'
         else:
-            print('Файлов пока что нет')
-    except:
-        print('Произошла ошибка')
-
-
-def delete_all_notes():  # Удаление всех файлов
-    try:
-        notes = [note for note in os.listdir() if note.endswith('.txt')]
-        if notes:
-            for n in notes:
-                delete_note(n)
-            print('Все заметки удалены!')
-        else:
-            print('Файлов пока что нет')
-    except:
-        print('Произошла ошибка')
-        return True
-
-
-def main():  # объединение и вызовы функций
-    try:
-        print('Выберете действие, написав цифру:', end='')
-        print('''
-    1 - создать заметку
-    2 - Прочесть заметку
-    3 - Изменить заметку
-    4 - Удалить заметку
-    5 - Посмотреть созданные файлы
-    6 - Удалить все файлы
-    7 - Выход''')
-        func = int(input('Ввод: '))
-        match func:
-            case 1:
-                create_note()
-                return True
-            case 2:
-                note_name = input('Какую заметку вы хотите прочесть?\n')
-                read_note(note_name)
-                return True
-            case 3:
-                note_name = input('Какую заметку вы хотите изменить?\n')
-                edit_note(note_name)
-                return True
-            case 4:
-                note_name = input('Какую заметку вы хотите удалить?\n')
-                delete_note(note_name)
-                return True
-            case 5:
-                display_notes()
-                return True
-            case 6:
-                output = input('Вы уверены? \n Да - "y" Нет - \"n\" ')
-                if output == 'y':
-                    delete_all_notes()
-                    return True
-                else:
-                    return True
-            case 7:
-                ...
-            case _:
-                print('Ты должен выбрать в диапозоне от 1 до 7 ;)')
-                return True
-    except:
-        print('Произошла ошибка!')
-        return True
-
-
-if __name__ == '__main__':
-    while main():
-        pass
+            return f'Заметок пока что нет'
+    except Exception as err:
+        return f'Произошла ошибка {err}'
