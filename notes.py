2f94b22d55d435edcce42ccf167d689dd7187dbc
S7acel <ovsyannikovmark11@gmail.com>
2024-01-06 18:32:26 +0500

    notes.py function remade

--- notes.py
+++ notes.py
@@ -1,34 +1,44 @@
+'''Модуль os необходим для того чтобы работать с файлами операционной системы'''
 import os
 
-def check_extension(f):  # Проверка на наличие '.txt' в конце
+
+def check_extension(f):
+    '''Проверка на наличие '.txt' в конце. Добавляет к названию файла .txt,
+     в случае его отсутствия'''
     try:
         if not f.endswith('.txt'):
             f += '.txt'
         return f
-    except:
-        print('Произошла ошибка')
+    except AttributeError as err:
+        print(f'Произошла ошибка {err}')
+        return None
 
 
-def build_note(note_name, note_text):  # билд заметки (Для функции create note)
+def build_note(note_name, note_text):
+    ''' создание заметки реализация происходит за счет менеджера контекста with open'''
     try:
         note_name = check_extension(note_name)
         with open(note_name, 'w', encoding='utf-8') as f:
             f.write(note_text)
         print(f'Файл {note_name} создан успешно!\n')
-    except:
-        print("Произошла ошибка.")
+    except IOError as err:
+        print(f"Произошла ошибка. {err}")
 
 
-def create_note():  # создание файла
+def create_note():
+    '''создание файла происходит за счет вызова функции build_note()
+    и спрашивания имени и текста заметки'''
     try:
-        Name = input('Введите название файла\n')
-        Text = input('Введите текст\n')
-        build_note(Name, Text)
-    except:
+        name = input('Введите название файла\n')
+        text = input('Введите текст\n')
+        build_note(name, text)
+    except (IOError, KeyboardInterrupt):
         print("Произошла ошибка.")
 
 
-def read_note(name):  # Прочтение файла
+def read_note(name):
+    '''Реализуется за счет менеджера контекста. Происходит проверка наличия файла в системе
+     и вывод текста, в случае его наличия'''
     try:
         name = check_extension(name)
         if os.path.isfile(name):
@@ -36,15 +46,17 @@ def read_note(name):  # Прочтение файла
                 text = f.read()
             print(text)
             return True
-        else:
-            print('Такого файла не существует.')
-            return False
-    except:
-        print('Произошла ошибка')
+        print('Такого файла не существует.')
+        return False
+    except IOError as err:
+        print(f'Произошла ошибка {err}')
         return False
 
 
 def edit_note(name):  # редактирование заметки
+    '''Происходит проверка посредством вызова функции read note.
+    Сначала выводит содержимое файла (в случае успеха)
+    и далее требует новый текст для изменения'''
     try:
         name = check_extension(name)
         if read_note(name):
@@ -54,11 +66,12 @@ def edit_note(name):  # редактирование заметки
                 print(f'Файл "{name}" Изменён успешно!')
         else:
             print('Что редактировать то? 😐')
-    except:
-        print('Произошла ошибка')
+    except IOError as err:
+        print(f'Произошла ошибка {err}')
 
 
-def delete_note(name):  # Удаление заметки
+def delete_note(name):
+    '''Удаление заметки за счет функции os.remove'''
     try:
         name = check_extension(name)
         if os.path.isfile(name):
@@ -66,38 +79,45 @@ def delete_note(name):  # Удаление заметки
             print(f'Заметка {name} удалена успешно')
         else:
             print('Нельзя удалить то, чего и так нет.')
-    except:
-        print('Произошла ошибка')
+    except IOError as err:
+        print(f'Произошла ошибка {err}')
 
 
-def display_notes():  # Вывод названий файлов в порядке увеличения длины
+def display_notes():
+    '''Вывод названий файлов в порядке увеличения длины'''
     try:
-        notes = [note for note in os.listdir() if note.endswith('.txt')]
+        # создание списка .txt файлов, найденных в директории
+        notes = [note for note in os.listdir()
+                 if note.endswith('.txt')]
         reversed_notes = sorted(notes, key=len)
         if notes:
             for i in reversed_notes:
                 print(i)
         else:
             print('Файлов пока что нет')
-    except:
-        print('Произошла ошибка')
+    except IOError as err:
+        print(f'Произошла ошибка {err}')
 
 
-def delete_all_notes():  # Удаление всех файлов
+def delete_all_notes():
+    '''Удаление всех заметок'''
     try:
-        notes = [note for note in os.listdir() if note.endswith('.txt')]
+        # создание списка .txt файлов, найденных в директории
+        notes = [note for note in os.listdir()
+                 if note.endswith('.txt')]
         if notes:
             for n in notes:
                 delete_note(n)
             print('Все заметки удалены!')
         else:
             print('Файлов пока что нет')
-    except:
-        print('Произошла ошибка')
-        return True
+    except IOError as err:
+        print(f'Произошла ошибка {err}')
 
 
-def main():  # объединение и вызовы функций
+def main():
+    ''' объединение и вызовы функций'''
+    result = True
     try:
         print('Выберете действие, написав цифру:', end='')
         print('''
@@ -112,37 +132,30 @@ def main():  # объединение и вызовы функций
         match func:
             case 1:
                 create_note()
-                return True
             case 2:
                 note_name = input('Какую заметку вы хотите прочесть?\n')
                 read_note(note_name)
-                return True
             case 3:
                 note_name = input('Какую заметку вы хотите изменить?\n')
                 edit_note(note_name)
-                return True
             case 4:
                 note_name = input('Какую заметку вы хотите удалить?\n')
                 delete_note(note_name)
-                return True
             case 5:
                 display_notes()
-                return True
             case 6:
                 output = input('Вы уверены? \n Да - "y" Нет - \"n\" ')
                 if output == 'y':
                     delete_all_notes()
-                    return True
-                else:
-                    return True
             case 7:
                 ...
             case _:
                 print('Ты должен выбрать в диапозоне от 1 до 7 ;)')
-                return True
-    except:
-        print('Произошла ошибка!')
-        return True
+    except (ValueError, KeyboardInterrupt) as err:
+        print(f'Произошла ошибка! {err}')
+        result = True
+    return result
+
 
 
 if __name__ == '__main__':
