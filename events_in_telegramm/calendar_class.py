7ad0481e6df0c1db3b333a9e741808856bbbf449
S7acel <ovsyannikovmark11@gmail.com>
2024-02-21 18:59:03 +0500

    В файле calendar_class.py - добавлены методы adding_event_to_file и create_and_add_event_to_file. Так же в классе telegramm_event_hand.py - переделаны эти методы под эти классы. Эти методы работают таким образом, что не нуждаются в постоянных перезаписях. Так же добавлен файл events.jsonl, который имеет в себе формат json lines

--- events_in_telegramm/calendar_class.py
+++ events_in_telegramm/calendar_class.py
@@ -6,6 +6,39 @@ class Calendar:
     """Этот класс - связка методов для хранения, чтения, изменения и удаления событий.
         Сами методы - не связаны с классом с точки зрения логики. Их можно достать из класса"""
 
+    events = {}
+    event_id = 1
+
+    @classmethod
+    def adding_event_to_file(cls, dict_event):
+        """Этот метод - добавляет в файл 'events.jsonl' словарь-событие"""
+
+        try:
+            with open('events.jsonl', 'a', encoding='utf-8') as file:
+                json.dump(dict_event, file)
+                file.write('\n')
+        except FileNotFoundError as err:
+            print(f'Файл events.json - не существует {err}')
+
+    @classmethod
+    def create_and_add_event_to_file(cls, event_name, details, event_date, event_time, chat_id):
+        """В этом методе - событие добавляется в словарь и в json файл,
+        НО, в отличие от create_event, без повторной перезаписи файлов"""
+
+        event = {
+            'name': event_name,
+            'details': details,
+            'date': event_date,
+            'time': event_time,
+            'id': chat_id
+        }
+
+        cls.event_id += 1
+        cls.events[cls.event_id] = event
+        dict_event = {cls.event_id: event}
+        cls.adding_event_to_file(dict_event)
+        return cls.event_id
+
     @classmethod
     def check_missing_id(cls):
         """Этот метод - проверяет недостающие Id из списка. Добавляет их по порядку,
@@ -19,7 +52,7 @@ class Calendar:
         return len(cls.events) + 1
 
     @classmethod
-    def update_events(cls):
+    def update_all_events(cls):  # TODO переделать под формат json lines
         """Обновляет список событий из файла events.json, вытягивает их"""
         try:
             with open('events.json', 'r', encoding='utf-8') as file:
@@ -28,7 +61,7 @@ class Calendar:
             print(f'file events.json was not found {err}')
 
     @classmethod
-    def write_events_to_file(cls):  # записывает события в файл events.json
+    def write_all_events_to_file(cls):  # записывает события в файл events.json
         """Добавление события в словарь self.events. Номер события (id) - его ключ"""
 
         try:
@@ -41,7 +74,7 @@ class Calendar:
     def create_event(cls, event_data):
         """Создание события, сразу достает информацию из файла и записывает её обратно
          с помощью функций cls.update_events() и cls.write_events_to_file()"""
-        cls.update_events()
+        cls.update_all_events()
         event_id = cls.check_missing_id()
 
         event = {
@@ -52,14 +85,14 @@ class Calendar:
             'chat_id': event_data['chat_id']
         }
         cls.events[event_id] = event
-        cls.write_events_to_file()
+        cls.write_all_events_to_file()
         return event_id
 
     @classmethod
     def return_user_events(cls, chat_id):
         """Возвращает список всех событий пользователя (включая id и сами события)
         События находятся за счет chat_id пользователя"""
-        cls.update_events()
+        cls.update_all_events()
         user_events = []
         for event in cls.events:
             if cls.events[event]['chat_id'] == chat_id:
@@ -67,13 +100,14 @@ class Calendar:
         return user_events
 
     @classmethod
+    # TODO Переделать удаление не за счет перезаписи всего файла
     def delete_user_events(cls, chat_id, id_of_event):
         """Удаляет элемент словаря, проверка принадлежности события происходит
          за счет сравнения chat id"""
-        cls.update_events()
+        cls.update_all_events()
         if cls.events[id_of_event]['chat_id'] == chat_id:
             del cls.events[id_of_event]
-            cls.write_events_to_file()
+            cls.write_all_events_to_file()
             return f'Событие под номером {id_of_event} - удалено успешно!'
         return "Произошла ошибка. Возможно, такого события - не существует"
 
@@ -82,9 +116,9 @@ class Calendar:
         """Редактирование элемента события. Сначала обновляются все события,
         а потом с проверяем принадлежность и уже изменяем его,
         так же записываем изменения"""
-        cls.update_events()
+        cls.update_all_events()
         if cls.events[id_of_event]['chat_id'] == chat_id:
             cls.events[id_of_event][edit_object] = new_edit_object
-            cls.write_events_to_file()
+            cls.write_all_events_to_file()
             return f'Событие под номером {id_of_event} - изменено успешно!'
         return "Произошла ошибка. Возможно, такого события - не существует"
